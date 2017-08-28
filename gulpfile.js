5b907dda28277ef42e04d39248c1a86172aef069
Kate <burdiyak28@gmail.com>
2017-08-28 20:17:35 +0200

    add koala

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp'), // Подключаем Gulp
+var gulp = require('gulp'); // Подключаем Gulp
     sass = require('gulp-sass'); // Подключаем Sass пакет
+    autoprefixer = require ('gulp-autoprefixer'); // Подключаем autoprefixer
 
 gulp.task('sass', function() { // Создаем таск "sass"
   return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('css')) // Выгружаем результата в папку css
-  });
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefix({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
+    .pipe(gulp.dest('css'));
+});
 
 gulp.task('watch', function() {
   gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
